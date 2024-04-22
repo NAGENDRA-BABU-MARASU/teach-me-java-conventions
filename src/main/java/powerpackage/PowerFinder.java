c02b50d4a855c2d3863ea520945dcfc32ced940c
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-22 09:49:26 +0530

    made some improvements to the tests

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -3,7 +3,7 @@ package powerpackage;
 public class PowerFinder {
     public static int findPowerFor(int base, int exponent) {
         int power = 1;
-        for (int i = 0; i<exponent; i++) {
+        for (int i = 0; i < exponent; i++) {
             power *= base;
         }
         return power;
--- src/test/java/powerpackage/PowerFinderTest.java
+++ src/test/java/powerpackage/PowerFinderTest.java
@@ -6,29 +6,37 @@ import org.junit.Test;
 public class PowerFinderTest {
     @Test
     public void shouldReturnOneWhenOneRaisedToOne() {
-        int onePowerOne = PowerFinder.findPowerFor(1, 1);
         int expectedResult = 1;
+
+        int onePowerOne = PowerFinder.findPowerFor(1, 1);
+
         assert onePowerOne == expectedResult;
     }
 
     @Test
     public void shouldReturnTwoOneWhenTwoRaisedToOne() {
-        int twoPowerOne = PowerFinder.findPowerFor(2, 1);
         int expectedResult = 2;
+
+        int twoPowerOne = PowerFinder.findPowerFor(2, 1);
+
         assert twoPowerOne == expectedResult;
     }
 
     @Test
     public void shouldReturnFourWhenTwoRaisedToTwo() {
-        int twoPowerTwo = PowerFinder.findPowerFor(2, 2);
         int expectedResult = 4;
+
+        int twoPowerTwo = PowerFinder.findPowerFor(2, 2);
+
         assert twoPowerTwo == expectedResult;
     }
 
     @Test
     public void shouldReturnSixWhenThreeRaisedToTwo() {
-        int threePowerTwo = PowerFinder.findPowerFor(3, 2);
         int expectedResult = 9;
+
+        int threePowerTwo = PowerFinder.findPowerFor(3, 2);
+
         assert expectedResult == threePowerTwo;
     }
 }
